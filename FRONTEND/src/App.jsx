a00aa7e03fcb1ed035bd4d0380f560b36aa16c65
madarawarakagoda <madarawarakagoda8@gmail.com>
2024-09-17 22:18:48 +0530

    create pages

---
+++ FRONTEND/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./App.css"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <>
+    <Router>
+      <Routes>
+          <Route/>
+      </Routes>
+    </Router>
+    </>
+  )
+}
+
+export default App
