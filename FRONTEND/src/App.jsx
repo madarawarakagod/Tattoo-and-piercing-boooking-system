5221475a49ccc308f27e4dde581ecb2c6a94979a
madarawarakagoda <madarawarakagoda8@gmail.com>
2024-09-18 00:16:49 +0530

    update app

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import "./App.css"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Appointment from "./Pages/Appointment";
+import Register from "./pages/Register";
+import AboutUs from "./Pages/AboutUs";
+import Login from "./Pages/Login";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
     <>
     <Router>
       <Routes>
-          <Route/>
+      <Route path="/" element={<Home />} />
+          <Route path="/appointment" element={<Appointment />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
       </Routes>
+      <ToastContainer/>
     </Router>
     </>
   )
