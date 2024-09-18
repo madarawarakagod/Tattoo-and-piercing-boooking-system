ad06f0911347574fdcb6ffcb4d54559692fe6d5c
madarawarakagoda <madarawarakagoda8@gmail.com>
2024-09-18 19:57:39 +0530

    delete

--- FRONTEND/src/pages/Home.jsx
+++ FRONTEND/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-//import React from "react";
+import React from "react";
 import Hero from "../components/Hero";
 import Biography from "../components/Biography";
 import MessageForm from "../components/MessageForm";
