4e4b7c9cb7cd7044fc93433a30404b11b77e13c7
madarawarakagoda <madarawarakagoda8@gmail.com>
2024-09-18 14:10:30 +0530

    changes

--- FRONTEND/src/components/Hero.jsx
+++ FRONTEND/src/components/Hero.jsx
@@ -1,33 +1,29 @@
-import PropTypes from 'prop-types';
+import React from "react";
 
 const Hero = ({ title, imageUrl }) => {
   return (
-    <div className="hero container">
-      <div className="banner">
-        <h1>{title}</h1>
-        <p>
-          ZeeCare Medical Institute is a state-of-the-art facility dedicated
-          to providing comprehensive healthcare services with compassion and
-          expertise. Our team of skilled professionals is committed to
-          delivering personalized care tailored to each  patient&apos;s needs. At
-          ZeeCare, we prioritize your well-being, ensuring a harmonious
-          journey towards optimal health and wellness.
-        </p>
+    <>
+      <div className="hero container">
+        <div className="banner">
+          <h1>{title}</h1>
+          <p>
+            ZeeCare Medical Institute is a state-of-the-art facility dedicated
+            to providing comprehensive healthcare services with compassion and
+            expertise. Our team of skilled professionals is committed to
+            delivering personalized care tailored to each patient's needs. At
+            ZeeCare, we prioritize your well-being, ensuring a harmonious
+            journey towards optimal health and wellness.
+          </p>
+        </div>
+        <div className="banner">
+          <img src={imageUrl} alt="hero" className="animated-image" />
+          <span>
+            <img src="/Vector.png" alt="vector" />
+          </span>
+        </div>
       </div>
-      <div className="banner">
-        <img src={imageUrl} alt="hero" className="animated-image" />
-        <span>
-          <img src="/2.jpg" alt="vector" />
-        </span>
-      </div>
-    </div>
+    </>
   );
-};
-
-// Adding PropTypes for validation
-Hero.propTypes = {
-  title: PropTypes.string.isRequired,
-  imageUrl: PropTypes.string.isRequired
 };
 
 export default Hero;
