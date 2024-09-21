8ab19766b635e4657504a75b6a1ab6f3fe724d15
madarawarakagoda <madarawarakagoda8@gmail.com>
2024-09-21 12:25:12 +0530

    create routers

---
+++ backend/router/userRouter.js
@@ -0,0 +1,10 @@
+import express from "express";
+import {patientRegister} from "../controller/userController.js";
+
+const router =express.Router();
+
+router.post("/patient/register",patientRegister);
+router.post("/login",login);
+router.post("/admin/addnew",addNewAdmin);
+
+export default router;
