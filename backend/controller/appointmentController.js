10d10b1c381558b84590cbdd03379ee2ce40e7bf
madarawarakagoda <madarawarakagoda8@gmail.com>
2024-09-21 20:52:36 +0530

    update

--- backend/controller/appointmentController.js
+++ backend/controller/appointmentController.js
@@ -1,5 +1,5 @@
 import { catchAsyncErrors } from "../middlewares/catchAsyncErrors.js";
-import ErrorHandler from "../middlewares/error.js";
+import ErrorHandler from "../middlewares/errorMiddleware.js";
 import { Appointment } from "../models/appointmentSchema.js";
 import { User } from "../models/userSchema.js";
 
--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,6 +1,6 @@
 import { catchAsyncErrors } from "../middlewares/catchAsyncErrors.js";
 import { User } from "../models/userSchema.js";
-import ErrorHandler from "../middlewares/error.js";
+import ErrorHandler from "../middlewares/errorMiddleware.js";
 import { generateToken } from "../utils/jwtToken.js";
 import cloudinary from "cloudinary";
 
@@ -20,8 +20,8 @@ export const patientRegister = catchAsyncErrors(async (req, res, next) => {
       return next(new ErrorHandler("Please Fill Full Form!", 400));
     }
 
-    let user = await User.findOne({ email });
-    if (user) {
+    const isRegistered = await User.findOne({ email });
+    if (isRegistered) {
       return next(new ErrorHandler("User already Registered!", 400));
     }
     const user = await User.create({
